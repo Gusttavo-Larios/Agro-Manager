d909ca541504ba0c3a6e80fc89848b03e6a90327
dev <gustavolarios077@gmail.com>
2023-12-09 12:47:07 -0400

    refactor: Lidando com execeções na craição de agricultor.

--- api/src/farmer/farmer.repository.ts
+++ api/src/farmer/farmer.repository.ts
@@ -1,43 +1,51 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, InternalServerErrorException } from "@nestjs/common";
 
 import { PrismaService } from "src/prisma.service";
 import { CreateFarmerDto } from "./dto/create-farmer.dto";
 import { Farmer } from "./entities/farmer.entity";
+import { Prisma } from "@prisma/client";
 
 @Injectable()
 export class FarmerRepository {
     constructor(private prisma: PrismaService) { }
 
     async create(createFarmerDto: CreateFarmerDto): Promise<Farmer> {
-        return await this.prisma.farmer.create({
-            data: {
-                corporate_name: createFarmerDto.corporate_name,
-                fantasy_name: createFarmerDto.fantasy_name,
-                company_identification: createFarmerDto.company_identification,
-                phone_number: createFarmerDto.phone_number,
-                city: {
-                    connectOrCreate: {
-                        where: {
-                            ibge_code: createFarmerDto.city.ibge_code
-                        },
-                        create: {
-                            city_name: createFarmerDto.city.city_name,
-                            ibge_code: createFarmerDto.city.ibge_code,
-                            state: {
-                                connect: {
-                                    id: createFarmerDto.city.state_id
+        try {
+            return await this.prisma.farmer.create({
+                data: {
+                    corporate_name: createFarmerDto.corporate_name,
+                    fantasy_name: createFarmerDto.fantasy_name,
+                    company_identification: createFarmerDto.company_identification,
+                    phone_number: createFarmerDto.phone_number,
+                    city: {
+                        connectOrCreate: {
+                            where: {
+                                ibge_code: createFarmerDto.city.ibge_code
+                            },
+                            create: {
+                                city_name: createFarmerDto.city.city_name,
+                                ibge_code: createFarmerDto.city.ibge_code,
+                                state: {
+                                    connect: {
+                                        id: createFarmerDto.city.state_id
+                                    }
                                 }
                             }
                         }
-                    }
-                },
-                createdBy: {
-                    connect: {
-                        id: createFarmerDto.created_by
+                    },
+                    createdBy: {
+                        connect: {
+                            id: createFarmerDto.created_by
+                        }
                     }
                 }
+            })
+        } catch (error) {
+            if(error instanceof Prisma.PrismaClientKnownRequestError) {
+                throw new BadRequestException(error.message)
             }
-        })
+            throw new InternalServerErrorException(error.message)
+        }
     }
 
     async findAll(page?: number): Promise<Farmer[]> {
