46697cedca46221e4b834e32919a0a4bcad2bc54
dev <gustavolarios077@gmail.com>
2023-12-09 13:12:34 -0400

    refacotr: Ajuste na validação do campo company_identification.

--- api/src/farmer/dto/create-farmer.dto.ts
+++ api/src/farmer/dto/create-farmer.dto.ts
@@ -29,7 +29,7 @@ export class CreateFarmerDto {
     @IsString()
     @IsNotEmpty()
     @MaxLength(18)
-    @Matches(/(\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2})|(\d{3}\.\d{3}\.\d{3}\-\d{2})/g, {
+    @Matches(/(^\d{3}\.\d{3}\.\d{3}\-\d{2}$)|(^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$)/, {
         message: "A identificação da empresa deve seguir um dos seguintes formatos: 99.999.999/9999-99 ou 999.999.999-99"
     })
     company_identification: string;
