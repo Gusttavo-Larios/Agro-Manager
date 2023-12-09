6c3b201bb8cf85b7a8ee6fabbbd33b4ab37aafb7
dev <gustavolarios077@gmail.com>
2023-12-09 12:34:02 -0400

    feat: Validação do formulário de criação de agricultor.

--- api/src/city/dto/create-city.dto.ts
+++ api/src/city/dto/create-city.dto.ts
@@ -1,5 +1,38 @@
+import { IsNotEmpty, IsNumber, IsPositive, IsString, Length, MaxLength } from "class-validator";
+
 export class CreateCityDto {
+    @IsString({
+        message: "O nome da cidade dever ser uma cadeia de caracteres."
+    })
+    @IsNotEmpty({
+        message: "O nome da cidade é requerido."
+    })
+    @MaxLength(64, {
+        message: "O nome da cidade deve ter no máximo 64 caracteres."
+    })
     city_name: string;
+
+    @IsNumber({
+        maxDecimalPlaces: 0
+    }, {
+        message: "A referência ao estado deve ser um número inteiro."
+    })
+    @IsPositive(
+        {
+            message: "A referência ao estado deve ser um número positivo."
+        }
+    )
+    @IsNotEmpty({
+        message: "O id do estado é requerido."
+    })
     state_id: number;
+
+    @IsString()
+    @IsNotEmpty({
+        message: "O código do ibge da cidade é requerido."
+    })
+    @Length(7, 7, {
+        message: "O código do ibge da cidade deve ter 7 caracteres."
+    })
     ibge_code: string;
 }
--- api/src/farmer/dto/create-farmer.dto.ts
+++ api/src/farmer/dto/create-farmer.dto.ts
@@ -1,11 +1,43 @@
 import { CreateCityDto } from "src/city/dto/create-city.dto";
+import { IsEmail, IsInstance, IsNotEmpty, IsNumber, IsPhoneNumber, IsPositive, IsString, Matches, MaxLength, ValidateNested } from 'class-validator';
+import { Type } from "class-transformer";
 
 export class CreateFarmerDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(96)
     corporate_name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(96)
     fantasy_name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(18)
+    @Matches(/(\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2})|(\d{3}\.\d{3}\.\d{3}\-\d{2})/g, {
+        message: "A identificação da empresa deve seguir um dos seguintes formatos: 99.999.999/9999-99 ou 999.999.999-99"
+    })
     company_identification: string;
+
+    @IsPhoneNumber("BR")
     phone_number: string;
+
+    @ValidateNested()
+    @Type(() => CreateCityDto)
     city: CreateCityDto;
+
+    @IsNumber({
+        maxDecimalPlaces: 0
+    }, {
+        message: "O identificador do criador do cadastro do agricultor dever ser um número."
+    })
+    @IsPositive({
+        message: "O identificador do criador do cadastro do agricultor dever ser um número positivo."
+    })
+    @IsNotEmpty({
+        message: "O identificador do criador do cadastro é requerido."
+    })
     created_by: number
-    state_id: number
 }
