d50fae23a9d5fc38ff16042ff0a0d76f3862a70a
dev <gustavolarios077@gmail.com>
2023-12-12 10:54:04 -0400

    feat: Adição do botão de voltar

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,34 +1,55 @@
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import { ChevronDownIcon, ChevronLeftIcon } from "@chakra-ui/icons";
 import {
   Button,
   HStack,
+  IconButton,
   Image,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
+  VStack,
 } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
 
-import IconSmall from "@/assets/imgs/ICON_SMALL.svg";
+import { router } from "@/router";
+import { PathsEnum } from "@/enums/path.enum";
 import { SystemUtil } from "@/utils/system.util";
 
+import IconSmall from "@/assets/imgs/ICON_SMALL.svg";
+
 export function Header() {
+  const { pathname } = useLocation();
   return (
-    <HStack
-      pb="1rem"
-      w="100%"
-      alignItems="center"
-      justifyContent="space-between"
-    >
-      <Image src={IconSmall} />
-      <Menu>
-        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-          Menu
-        </MenuButton>
-        <MenuList>
-          <MenuItem onClick={SystemUtil.signOut}>Sair</MenuItem>
-        </MenuList>
-      </Menu>
-    </HStack>
+    <VStack w="100%" alignItems={"flex-start"}>
+      <HStack
+        pb="1rem"
+        w="100%"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Image src={IconSmall} />
+        <Menu>
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+            Menu
+          </MenuButton>
+          <MenuList>
+            <MenuItem onClick={SystemUtil.signOut}>Sair</MenuItem>
+          </MenuList>
+        </Menu>
+      </HStack>
+      {pathname !== PathsEnum.FARMERS && (
+        <IconButton
+          isRound={true}
+          variant="outline"
+          colorScheme="gray"
+          color={"black"}
+          aria-label="Done"
+          fontSize="20px"
+          icon={<ChevronLeftIcon />}
+          onClick={() => router.navigate(-1)}
+        />
+      )}
+    </VStack>
   );
 }
---
+++ client/src/enums/path.enum.ts
@@ -0,0 +1,7 @@
+export enum PathsEnum {
+    FARMERS = "/agricultores",
+    NEW_FARMER = "/agricultor/novo",
+    UPDATE_FARMER = "/agricultor/",
+    LOGIN = "/",
+  }
+  
